ae111a2f163c2f2c0d213fc8e8d4c92f943db2b4
Zain Ullah <zainullahachakzai@gmail.com>
2025-06-25 19:48:55 +0500

    Fix base path for Netlify

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/ChefX/",
+  base: '/',
   plugins: [react()],
-})
+});
+
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   base: "/ChefX/",
+//   plugins: [react()],
+// })
